bef32dc4917d2d244a8673bf6370e2c966c7d398
Jacobo de Vera <devel@jacobodevera.com>
2024-12-10 10:38:02 +0000

    📦 Support a --version flag for the built bundle

--- templates/footer.sh
+++ templates/footer.sh
@@ -1,5 +1,14 @@
 # shellcheck shell=bash
 
 if ! code::is_sourced; then
-    head -n 20 "$0" | sed -n '2,20p' | sed -e 's/^# //' -e 's/^#//'
+    case "$1" in
+        --version | -v)
+            echo "$__JDVLIB_VERSION"
+            exit 0
+            ;;
+        *)
+            head -n 20 "$0" | sed -n '2,20p' | sed -e 's/^# //' -e 's/^#//'
+            exit 0
+            ;;
+    esac
 fi
